322832bde4434161f1e02b0e23bd58135db3a363
alexandre-ls94 <xandiie.ls@gmail.com>
2020-02-21 10:58:13 -0300

    Projeto Peoples

---
+++ senai-peoples-02-DML.sql
@@ -0,0 +1,5 @@
+USE M_Peoples;
+
+INSERT INTO Funcionarios (Nome, Sobrenome, DataNascimento)
+VALUES	('Catarina','Strada','20-01-2000'),
+		('Tadeu','Vitelli','19-02-2000')
---
+++ senai.Peoples.WebApi/senai.Peoples.WebApi/Interfaces/IFuncionarioRepository.cs
@@ -0,0 +1,23 @@
+﻿using senai.Peoples.WebApi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai.Peoples.WebApi.Interfaces
+{
+    interface IFuncionarioRepository
+    {
+        List<FuncionarioDomain> Listar();
+
+        FuncionarioDomain BuscarPorId(int id);
+
+        void Inserir(FuncionarioDomain funcionario);
+
+        void Atualizar(FuncionarioDomain funcionario);
+
+        void Deletar(int id);
+
+        FuncionarioDomain BuscarPorNome(string nome);
+    }
+}
