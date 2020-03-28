3351e964d97037bb273b8974254036ecb6d2f654
alexandre-ls94 <xandiie.ls@gmail.com>
2020-03-27 22:56:21 -0300

    update BD-DQL

--- BD/senai-peoples-03-DQL.sql
+++ BD/senai-peoples-03-DQL.sql
@@ -3,3 +3,7 @@ USE M_Peoples;
 SELECT * FROM TipoUsuario;
 SELECT * FROM Usuario;
 SELECT * FROM Funcionarios;
+
+SELECT CONCAT (Nome ,' ', Sobrenome) AS NomeCompleto FROM Funcionarios;
+
+SELECT * FROM Funcionarios ORDER BY Nome DESC;
